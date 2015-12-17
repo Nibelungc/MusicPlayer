1e44266fabfc35febb58a1fe7e00a389d9998a3e
Nikolay Kagala <nick@sequenia.com>
2015-12-17 10:36:32 +0600

    Initial Commit

---
+++ MusicPlayer/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MusicPlayer
+//
+//  Created by Denis Baluev on 17/12/15.
+//  Copyright © 2015 Sequenia. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MusicPlayer/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MusicPlayer
+//
+//  Created by Denis Baluev on 17/12/15.
+//  Copyright © 2015 Sequenia. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
